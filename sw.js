d20876e3905e54aba4951d0adfcee57efcd3a53f
epkmartins-rgb <e.pkmartins@gmail.com>
2026-06-04 17:41:26 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'precomestre-v2';
+const ASSETS = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Nunca interceptar chamadas externas (Google Sheets, APIs, etc)
+  if (!e.request.url.startsWith(self.location.origin)) return;
+  
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
